cf5253bdc20cc06efd018b057dfc20dfd094948d
Aarick Lameman <lamemanaarick@gmail.com>
2016-05-19 11:24:54 -0600

    ecnchurchdb.sql updated database

--- ecnchurchdb.sql
+++ ecnchurchdb.sql
@@ -1,13 +1,24 @@
 USE ecnchurch;
 DROP TABLE IF EXISTS bulletins;
+DROP TABLE IF EXISTS programs;
 DROP TABLE IF EXISTS members;
 DROP TABLE IF EXISTS missions;
 
-
-
 CREATE TABLE missions (
   missionsId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+  address1 VARCHAR (32) NOT NULL,
+  address2 VARCHAR (32) NOT NULL,
+  activation CHAR(16),
+  city VARCHAR (32) NOT NULL,
   description VARCHAR(32) NOT NULL,
+  email VARCHAR(128) NOT NULL,
+  firstName VARCHAR(32) NOT NULL,
+  lastName VARCHAR(32) NOT NULL,
+  pic VARCHAR(32) NOT NULL,
+  phone VARCHAR(32) NOT NULL,
+  servicetime VARCHAR (128) NOT NULL,
+  state VARCHAR (32) NOT NULL,
+  zip VARCHAR (32) NOT NULL,
   PRIMARY KEY(missionsId)
 );
 
@@ -18,7 +29,6 @@ CREATE TABLE members (
   activation CHAR(16),
   email VARCHAR(128) NOT NULL,
   firstName VARCHAR(32) NOT NULL,
-  hash CHAR(128) NOT NULL,
   lastName VARCHAR(32) NOT NULL,
   phone VARCHAR(32) NOT NULL,
   position VARCHAR(32) NOT NULL,
@@ -29,19 +39,36 @@ CREATE TABLE members (
   address2 VARCHAR (32) NOT NULL,
   gender VARCHAR (32) NOT NULL,
   dob DATE NOT NULL,
-  salt CHAR(64) NOT NULL,
   INDEX(missionsId),
   FOREIGN KEY(missionsId) REFERENCES missions(missionsId),
   PRIMARY KEY(membersId)
 
 );
+
+CREATE TABLE programs (
+  programsId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+  missionsId INT UNSIGNED NOT NULL,
+  activation CHAR(16),
+  date DATE NOT NULL,
+  description VARCHAR(160) NOT NULL,
+  location VARCHAR(32) NOT NULL,
+  programName VARCHAR(32) NOT NULL,
+  time DATE NOT NULL,
+  INDEX(missionsId),
+  FOREIGN KEY(missionsId) REFERENCES missions(missionsId),
+  PRIMARY KEY(programsId)
+
+);
+
 CREATE TABLE bulletins (
   bulletinId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-  membersId INT UNSIGNED NOT NULL,
+  missionsId INT UNSIGNED NOT NULL,
   category VARCHAR(32) NOT NULL,
   message TEXT NOT NULL,
-  INDEX(membersId),
-  FOREIGN KEY (membersId) REFERENCES members(membersId),
+  timestamp TIMESTAMP NOT NULL,
+  INDEX(missionsId),
+  FOREIGN KEY (missionsId) REFERENCES missions(missionsId),
   PRIMARY KEY(bulletinId)
 
 );
+
