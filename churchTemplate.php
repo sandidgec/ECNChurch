02d64d8a2fb83facc7311702ead7e06694cbc7e2
Amber Lee <Lee.amber526@gmail.com>
2016-05-19 13:14:59 -0600

    added body

--- churchTemplate.php
+++ churchTemplate.php
@@ -5,6 +5,56 @@
     <title>WireFrame For ECN </title>
 </head>
 <body>
+<div>
+<nav class="navbar navbar-button navbar-fixed-top">
+    <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                div class="container">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand em-text" href="index.php">Home</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active">
+                <li><a href="index.php">History/Beliefs</a></li>
+                <li><a href="index.php">Ministries</a></li>
+                <li><a href="index.php">Governance</a></li>
+                <li><a href="index.php">Contact Us</a></li>
+                <li><a href="index.php">Support</a></li>
+                <li><a href="index.php">Current Events</a></li>
+            </ul>
 
+        </div><!--/.nav-collapse -->
+    </div>
+</div>
+</nav>
+<br> </br>
+    <h2>
+     <p>Ya'ateeh, Welcome to the Episcopal Church In Navajoland</p>
+    </h2>
+    <img src="../lib/img/elo_96868_traditions_lg_2.jpg">
+<br> </br>
+    <p> Episcopal Church of Navajoland is more than just a place of worship.
+    It is a growing, loving family. There are so many great opportunities here to make friends,
+    receive ministry and affect people's lives with God's love.
+    There is something here for everyone, and we invite you to take a look and see.
+    </p>
+<br> </br>
+    <div>
+        <img alt="File:Bulletin Board with notes.svg" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Bulletin_Board_with_notes.svg/800px-Bulletin_Board_with_notes.svg.png"
+         width="800" height="568" srcset="https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Bulletin_Board_with_notes.svg/1200px-Bulletin_Board_with_notes.svg.png 1.5x,
+         https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Bulletin_Board_with_notes.svg/1600px-Bulletin_Board_with_notes.svg.png 2x" data-file-width="1050"
+         data-file-height="745">
+    </div>
+<br> </br>
+<h1> <img src="">
+
+<p>Arizona Churches</p></h1>
+<link> 
 </body>
+
 </html>
