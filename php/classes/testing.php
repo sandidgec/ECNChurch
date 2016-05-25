2e0bfbefa4e52e9461262deaaa698aba3945aa92
Dana Yazzie <redcloudsaboveus01@gmail.com>
2016-05-25 15:12:08 -0600

    Finished API

--- php/classes/testing.php
+++ php/classes/testing.php
@@ -14,13 +14,10 @@ $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
 $pdo = new PDO($dsn, $config["username"], $config["password"], $options);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$miss = new Missions(null, "1 Kit Carson Drive", "Po Box 618", "Ft. Defiance", "gjgordy1996@yahoo.com", "(928) 729-2322", "Cynthia Hizer", "10:30", "AZ",
-   "86504");
-
-var_dump($miss);
+$prog = new Programs(null, 2, "10/15/2016", "Fundraising for the Church", " All Saints", "Fall Festival", "06:00:00");
 
 try {
-    $miss->insert($pdo);
+    $prog->insert($pdo);
 }catch (PDOException $exception) {
     echo $exception->getMessage();
 }
