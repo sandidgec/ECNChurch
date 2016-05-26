df482752757235b071381ceb55fcac96d5becd14
Aarick Lameman <lamemanaarick@gmail.com>
2016-05-26 09:58:43 -0600

    autoload update

--- php/classes/autoload.php
+++ php/classes/autoload.php
@@ -19,3 +19,21 @@ function loadClass($members) {
 }
 // tell PHP to use the loadClass() function to automatically load class files
 spl_autoload_register("loadClass");
+
+spl_autoload_register("Autoloader::classLoader");
+class Autoloader {
+    /**
+     * This function autoloads classes if they exist
+     *
+     * @param string $className name of class to load
+     * @return bool false if classes can not be loaded
+     **/
+    public static function classLoader($className) {
+        $className = strtolower($className);
+        if(is_readable(__DIR__ . "/$className.php")) {
+            require_once(__DIR__ . "/$className.php");
+        } else {
+            return (false);
+        }
+    }
+}
