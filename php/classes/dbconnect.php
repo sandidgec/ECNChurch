9c4bb575877d9df0d04324771f25e712fb69ad0c
Bjorkbat <bjorkbat@gmail.com>
2016-05-31 11:26:58 -0600

    added a nifty function for establishing a global database connection

---
+++ php/classes/dbconnect.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Pretty simple.  Contains a single function that can be reused for
+ * establishing a database connection via ini file.
+ * * * * */
+
+function establishConn($iniFile) {
+
+  if ($GLOBALS['dbConn'] != null) {
+    $db = parse_ini_file($iniFile);
+
+    $user = $db['user'];
+    $pass = $db['pass'];
+    $name = $db['name'];
+    $host = $db['host'];
+    $type = $db['type'];
+
+    $GLOBALS['dbConn'] = new PDO($type . ":host=" . $host . ";dbname=" . $name,
+      $user, $pass);
+  }
+
+}
+
+?>
