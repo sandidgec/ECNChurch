21f7e1700f8e658c0712a697161abd02b1a977f3
Amber Lee <Lee.amber526@gmail.com>
2016-06-01 13:30:59 -0600

    added image

--- php/History&Beliefs.php
+++ php/History&Beliefs.php
@@ -22,6 +22,15 @@
             <div class="col-md-3">
                 <div class="thumbnail"> <img src="../lib/img/imginsert.jpg"></div>
             </div>
+            <div class="col-md-3">
+                <div class="thumbnail"> <img src="../lib/img/imginsert.jpg"></div>
+            </div>
+            <div class="col-md-3">
+                <div class="thumbnail"> <img src="../lib/img/imginsert.jpg"></div>
+            </div>
+            <div class="col-md-3">
+                <div class="thumbnail"> <img src="../lib/img/imginsert.jpg"></div>
+            </div>
         </div>
     </div>
     </body>
