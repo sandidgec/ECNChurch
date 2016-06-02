4a167114ac29c3a3d158907cafd3c1595c400299
Aarick Lameman <lamemanaarick@gmail.com>
2016-06-02 10:34:25 -0600

    Added bluecornproject.php file. Also updated the adminPanel.php file with forms for bulletin and table for missions.

--- lib/angular/controllers/adminPanel.php
+++ lib/angular/controllers/adminPanel.php
@@ -21,7 +21,44 @@
 
             </div>
 
+            <!-- Tablee for Missions -->
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Address 1</th>
+                        <th scope="col">Address 2 (Optional)</th>
+                        <th scope="col">City</th>
+                        <th scope="col">State</th>
+                        <th scope="col">Zip Code</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Person In Charge</th>
+                        <th scope="col">Service Time</th>
+
+                    </tr>
+                </thead>
+                <tfoot>
+                </tfoot>
+                <tbody>
+                    <tr>
+                        <th id="St.-Christopher's">St. Christopher's</th>
+                        <th id="3309-E.-Hwy.-162">3309 E. Hwy. 162</th>
+                        <th id="Church">Church</th>
+                        <th id="Bluff">Bluff</th>
+                        <th id="Utah">Utah</th>
+                        <th id="84512">84512</th>
+                        <th id="435-672-2206s">435-672-2206</th>
+                        <th id="sampsonl74@yahoo.com">SampsonL74@yahoo.com</th>
+                        <th id="Leon-Sampson">Leon Sampson</th>
+                        <th id="9am">9 A.M.-10 A.M. Every Sunday</th>
+
+                    </tr>
+                </tbody>
+            </table>
+
         </section>
+
 
 
 
