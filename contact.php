0fe557530d806cd51e793253305134fb2bbb3f09
Dana Yazzie <redcloudsaboveus01@gmail.com>
2016-06-02 13:26:34 -0600

    added background pic for contact

--- contact.php
+++ contact.php
@@ -7,14 +7,16 @@
     <!-- Custom styles for this template -->
     <link href="/lib/css/style.css" rel="stylesheet">
     <style>
-        body {background-color: aqua;}
+        body {
+              background-image: url("/lib/img/backcontact.jpg");
+        }
     </style>
 </head>
 <body>
 
 
 
-<div class="jumbotron">
+
     <div class="container">
         <div class="row">
             <div class="firstline">
@@ -22,25 +24,274 @@
             </div>
         </div>
     </div>
-</div>
-<div class="info">
-    <div style="float: right"><img height="300" width="300" src="../lib/img/imginsert.jpg" align="left">  Clergy Name/Info</div>
-    <div style="float: right;"><img height="300" width="300" src="../lib/img/imginsert.jpg" align="left">  Clergy Name/Info</div>
-    <div style="float: right;"><img height="300" width="300" src="../lib/img/imginsert.jpg" align="left">  Clergy Name/Info</div>
-    <div style="float: right;"><img height="300" width="300" src="../lib/img/imginsert.jpg" align="left">  Clergy Name/Info</div>
 
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="row">
+                        <img src="/lib/img/dave.JPG" alt="Person"
+                             class="img-responsive img-circle center-block"/>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h2 class="text-center">Rt. Rev. David E. Bailey</h2>
+
+                            <p class="text-center">
+                                Bishop
+                            </p>
+
+                            <p class="text-center">
+                                <a href="">Email</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="row">
+                        <img src="../lib/img/imginsert.jpg" alt="Person"
+                             class="img-responsive img-circle center-block"/>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h2 class="text-center">Rev. Cynthia Hizer</h2>
+
+                            <p class="text-center">
+                                Vicar of Good Shepherd Mission
+                            </p>
+
+                            <p class="text-center">
+                                <a href="">Email</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="row">
+                        <img src="/lib/img/Paula.PNG" alt="Person"
+                             class="img-responsive img-circle center-block"/>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h2 class="text-center">Rev. Deacon Paula Henson</h2>
+
+                            <p class="text-center">
+                                Mission Developer
+                            </p>
+
+                            <p class="text-center">
+                                <a href="">Email</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="row">
+                <img src="../lib/img/imginsert.jpg" alt="Person"
+                     class="img-responsive img-circle center-block"/>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="text-center">Rev. Deacon Leon Sampson</h2>
+
+                    <p class="text-center">
+                        Deacon in charge of St. Christophers Mission
+                    </p>
+
+                    <p class="text-center">
+                        <a href="">Email</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="row">
+                <img src="../lib/img/imginsert.jpg" alt="Person"
+                     class="img-responsive img-circle center-block"/>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="text-center">Rev. Catherine Plummer</h2>
+
+                    <p class="text-center">
+                        Priest in charge of St. Mary's
+                    </p>
+
+                    <p class="text-center">
+                        <a href="">Email</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="row">
+                <img src="/lib/img/inez.PNG" alt="Person"
+                     class="img-responsive img-circle center-block"/>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="text-center">Rev. Inez Velarde</h2>
+
+                    <p class="text-center">
+                        Priest in charge of St. Luke's
+                    </p>
+
+                    <p class="text-center">
+                        <a href="">Email</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="row">
+                <img src="../lib/img/imginsert.jpg" alt="Person"
+                     class="img-responsive img-circle center-block"/>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="text-center">Rev. Chan de Anaya</h2>
+
+                    <p class="text-center">
+                        Canon Missioner for ECN
+                    </p>
+
+                    <p class="text-center">
+                        <a href="">Email</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="row">
+                <img src="/lib/img/cat.jpg" alt="Person"
+                     class="img-responsive img-circle center-block"/>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="text-center">Rev. Cathlena Plummer</h2>
+
+                    <p class="text-center">
+                        Priest in charge of All Saints
+                    </p>
+
+                    <p class="text-center">
+                        <a href="">Email</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="row">
+                <img src="/lib/img/cornelia.JPG" alt="Person"
+                     class="img-responsive img-circle center-block"/>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="text-center">Rev. Cornelia Eaton</h2>
+
+                    <p class="text-center">
+                        Info
+                    </p>
+
+                    <p class="text-center">
+                        <a href="">Email</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="row">
+                <img src="../lib/img/imginsert.jpg" alt="Person"
+                     class="img-responsive img-circle center-block"/>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="text-center">Rev. Carol Tookey</h2>
+
+                    <p class="text-center">
+                        Supply Priest
+                    </p>
+
+                    <p class="text-center">
+                        <a href="">Email</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="row">
+                <img src="../lib/img/imginsert.jpg" alt="Person"
+                     class="img-responsive img-circle center-block"/>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="text-center">Rev. Patricia Cook</h2>
+
+                    <p class="text-center">
+                        Supply Priest
+                    </p>
+
+                    <p class="text-center">
+                        <a href="">Email</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="row">
+                <img src="../lib/img/imginsert.jpg" alt="Person"
+                     class="img-responsive img-circle center-block"/>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="text-center"></h2>
+
+                    <p class="text-center">
+                        Info
+                    </p>
+
+                    <p class="text-center">
+                        <a href="">Email</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 <!-- Contact -->
 <section id="contact" class="four">
     <div class="container">
 
         <header>
-            <h2>Contact</h2>
+            <h2>Have any questions?</h2>
         </header>
 
-        <p>Have any questions?</p>
-
-        <form method="post" action="../contact/" class="col-md-6 col-md-offset-3" id="contactForm" name="contactForm">
+        <form method="post" action="contact.php" class="col-md-6 col-md-offset-3" id="contactForm" name="contactForm">
             <div class="form-group">
                 <label for="email">Email address</label>
                 <input type="email" class="form-control" id="email" name="email" placeholder="Email">
