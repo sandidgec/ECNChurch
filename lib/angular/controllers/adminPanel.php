a5f4d1e762abac75485cd2bd1cba6d16bb52b0c0
Aarick Lameman <lamemanaarick@gmail.com>
2016-06-02 09:42:33 -0600

    added bulletin form to adminPanel.php file

---
+++ lib/angular/controllers/adminPanel.php
@@ -0,0 +1,42 @@
+<!DOC TYPE html>
+<html>
+    <head>
+        <title>Admin Panel</title>
+    </head>
+
+        <body>
+
+        <section class="contact" id="contact">
+            <div class="contact-info">
+
+                <form>
+                    <div class="form-group">
+                        <label for="InputBulletin">Bulletin</label>
+                        <input type="bulletin" class="form-control" id="exampleInputBulletin" placeholder="Bulletin">
+                    </div>
+                    <div class="form-group">
+                        <label for="InputMessage">Message</label>
+                        <input type="message" class="form-control" id="exampleInputMessage" placeholder="message">
+                    </div>
+
+            </div>
+
+        </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </body>
+</html>
