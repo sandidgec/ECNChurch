e214d3fbc0b9f172671968b1034b8ece7e6a5605
Amber Lee <Lee.amber526@gmail.com>
2016-06-02 09:37:54 -0600

    added div tags in history&beliefs.php

--- History&Beliefs.php
+++ History&Beliefs.php
@@ -8,6 +8,7 @@
         <link href="/lib/css/style.css" rel="stylesheet">
     </head>
     <body>
+
         <div class="jumbotron">
             <div class="container">
                 <div class="row">
@@ -20,64 +21,62 @@
     
     </div>
 
-        <div class="container">
-            <div class="pagewrapper">
-                <div class="col-md-12">
+      
+        <!-- End wrap -->
+        <div id="footer">
 
+            <div class="container">
+                <div class="row" align="center">
+                    <div class="col-md-5">
 
-                    <div class="col-sm-4">
-                        <div id="accordion" class="panel-group">
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <h4 class="panel-title"><a href="#collapseOne" data-toggle="collapse" data-parent="#accordion"> We call ourselves ‘The Dine’ </a></h4>
-                                </div>
-                                <div id="collapseOne" class="panel-collapse collapse in">
-                                    <div class="panel-body">We call ourselves ‘The Dine’ - The People - and we are one of the largest Native American Indian tribes in the United States. We have lived in the Southwest for hundreds of years, with a culture embedded in the Divine Creator and a tradition of worship with roots deep in the earth.</div>
-                                </div>
-                            </div>
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <h4 class="panel-title"><a href="#collapseTwo" data-toggle="collapse" data-parent="#accordion"> Our relationship with the United States </a></h4>
-                                </div>
-                                <div id="collapseTwo" class="panel-collapse collapse">
-                                    <div class="panel-body">Our relationship with the United States government has been difficult: We were forcibly removed from our land in 1863 and, on The Long Walk, were marched hundreds of miles away. Our possessions were taken, our farmlands destroyed, and our livestock killed. Later, our children were taken from us, placed in boarding schools, and punished for speaking our language and practicing our culture. We have internalized much of this sorrow, and today, we experience high rates of addiction and suicide. Given our geography, unemployment and under-employment are high.</div>
-                                </div>
-                            </div>
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <h4 class="panel-title"><a href="#collapseThree" data-toggle="collapse" data-parent="#accordion"> We continue to serve proudly </a></h4>
-                                </div>
-                                <div id="collapseThree" class="panel-collapse collapse">
-                                    <div class="panel-body">Despite this, we continue to serve proudly in the United States military. Many of our brothers and sisters are currently in Iraq and Afghanistan. The Navajo Code Talkers of World War II are renowned for helping to win the war in the Pacific. Additionally, Navajo military enlistment is much higher than the national average.</div>
-                                </div>
-                            </div>
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <h4 class="panel-title"><a href="#collapseFour" data-toggle="collapse" data-parent="#accordion"> In 1894, The Episcopal Church ... </a></h4>
-                                </div>
-                                <div id="collapseFour" class="panel-collapse collapse">
-                                    <div class="panel-body">In 1894, The Episcopal Church began a medical mission here. In 1978, The Episcopal Church founded the Area Mission on the 27,000 square miles in Arizona, New Mexico, and Utah, which are contiguous with the Navajo Nation. Until recently, our buildings were falling into disrepair and leadership and vision came from non-native clergy.</div>
-                                </div>
-                            </div>
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <h4 class="panel-title"><a href="#collapseFive" data-toggle="collapse" data-parent="#accordion"> There is a new spirit ... </a></h4>
-                                </div>
-                                <div id="collapseFive" class="panel-collapse collapse">
-                                    <div class="panel-body">Today, there is a new spirit and a new energy in our land! We have re-opened some churches and planted new ones. We have ordained two Navajo priests and three deacons. We have reached out to the many Navajo military veterans. We have repaired derelict properties for rental income and program use. We are exploring sustainable farming and aquaponics, expanding retreat opportunities and building hogans for educational ceremonial purposes. These efforts derive from our visioning together as we work to achieve our potential. Ultimately, these new beginnings will lead to a fully indigenous clergy, economic sustainability, and a deeper appreciation of the unique gifts we bring to the wider church.</div>
-                                </div>
-                            </div>
-                        </div>
+                        <img class="img-responsive" src="/designs/navajoland/img/logo_navajoland_long.png" alt="logo_small_navajoland">
+
                     </div>
 
+                    <div class="col-md-2">
+                        <ul class="socialiconsfooter">
+                            <li><a href="https://www.facebook.com/episcopalchurchinnavajolandecn" target="_blank"><img border="0" class="img-responsive" src="http://s3.amazonaws.com/dfc_attachments/images/3288810/icon_fb_web.png" alt="facebook"></a></li>
+                            <li><a href="https://twitter.com/navajoland" target="_blank"><img border="0" class="img-responsive" src="http://s3.amazonaws.com/dfc_attachments/images/3288806/icon_twitter_web.png" alt="twitter"></a></li>
+                            <li><a href="http://www.flickr.com/photos/episcopal-navajoland" target="_blank"><img border="0" class="img-responsive" src="http://s3.amazonaws.com/dfc_attachments/images/3288814/icon_flickr_web.png" alt="flickr"></a></li>
+                            <li><a href="http://vimeo.com/ecnvideos" target="_blank"><img border="0" class="img-responsive" src="http://s3.amazonaws.com/dfc_attachments/images/3288802/icon_vimeo_web.png" alt="vimeo"></a></li>
+                        </ul>
+                    </div>      <div class="col-sm-3">
+                        <small>
+                            <address>
+
+                                <a href="http://maps.google.com/?f=q&amp;hl=en&amp;q=P.O. Box 720 Farmington,&nbsp; NM 87499&amp;maptype=roadmap" target="_blank">Navajoland</a><br>
 
 
+                                <a href="http://maps.google.com/?f=q&amp;hl=en&amp;q=P.O. Box 720 Farmington,&nbsp; NM 87499&amp;maptype=roadmap" target="_blank">P.O. Box 720 <br> Farmington,&nbsp; NM 87499</a>
+
+                                <br>
+
+                                <span class="footerphone">Phone: </span>(505) 327-7549 — Main
+                                <br>
 
 
+                            </address>
+                        </small>
+                    </div>
+
+                    <div class="col-md-2"><img class="img-responsive" src="/designs/navajoland/img/logo_ec.png" alt="logo_ec"></div>
                 </div>
+
             </div>
+
         </div>
 
+
+
+        <!-- Bootstrap core JavaScript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="/designs/navajoland/js/jquery-1.10.2.min.js"></script>
+        <script src="/designs/navajoland/js/bootstrap.min.js"></script>
+
+
+
+        </body>
 
 
 
