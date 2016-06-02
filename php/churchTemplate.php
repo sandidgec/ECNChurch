665048e801560c2f9e36bc4f5c1f9dc91fc42afc
Aarick Lameman <lamemanaarick@gmail.com>
2016-06-02 08:39:07 -0600

    updated church template

--- php/churchTemplate.php
+++ php/churchTemplate.php
@@ -1,71 +1,78 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <?php require_once("../lib/head-utils.php");?>
+    <?php require_once("../lib/head-utils.php"); ?>
+    <?php require_once("navbar.php");?>
     <title>WireFrame For ECN </title>
     <!-- Custom styles for this template -->
     <link href="/lib/css/style.css" rel="stylesheet">
-    <link rel="short icon" href="../lib/img/ecn.ico"/>
-
 </head>
-
-<body ng-app="recall" ng-controller="recallCtrl">
-
-<?php require_once("navbar.php");?>
-
+<body>
 <div class="jumbotron">
+
     <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <h1>Ya'ateeh, Welcome to {{churchName}}</h1>
-            </div>
+
+        <div class="firstline">
+            <h2>Ya'a'teeh, Welcome to the Episcopal Church In Navajoland</h2>
         </div>
+
     </div>
+
 </div>
 
-    <div class="image1">
-        <img src="../lib/img/elo_96868_traditions_lg_2.jpg">
-    </div>
+<div class="imaginefirst">
+    <img src="/lib/img/elo_96868_traditions_lg_2.jpg">
+</div>
 
-<br> </br>
 
-    <p> Episcopal Church of Navajoland is more than just a place of worship.
-    It is a growing, loving family. There are so many great opportunities here to make friends,
-    receive ministry and affect people's lives with God's love.
-    There is something here for everyone, and we invite you to take a look and see.
-    </p>
+<div class="message">
+    <h3>The Episcopal Church in Navajoland is more than just a place of worship.</br>
+        It is a growing, loving family. There are so many great opportunities here to make friends,</br>
+        receive ministry and affect people's lives with God's love.</br>
+        There is something here for everyone, and we invite you to take a look and see.
+    </h3>
 
-<br> </br>
-
-    <div class="image1">
-        <img alt="File:Bulletin Board with notes.svg" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Bulletin_Board_with_notes.svg/800px-Bulletin_Board_with_notes.svg.png"
+</div>
+<p>Hello Guys</p>
+<div class="pictures">
+    <img alt="File:Bulletin Board with notes.svg" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Bulletin_Board_with_notes.svg/800px-Bulletin_Board_with_notes.svg.png"
          width="800" height="568" srcset="https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Bulletin_Board_with_notes.svg/1200px-Bulletin_Board_with_notes.svg.png 1.5x,
-         https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Bulletin_Board_with_notes.svg/1600px-Bulletin_Board_with_notes.svg.png 2x" data-file-width="1050"
+                     https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Bulletin_Board_with_notes.svg/1600px-Bulletin_Board_with_notes.svg.png 2x" data-file-width="1050"
          data-file-height="745">
-    </div>
+</div>
 
-<br> </br>
-
-        <div class="row">
-            <div class="col-md-4">
-                <div class="thumbnail">
-                    <img src="../../lib/img/arizona.jpg"><p>Arizona Churches</p>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="thumbnail">
-                    <img src="../../lib/img/newmex.jpg"><p>New Mexico Churches</p>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="thumbnail">
-                    <img src="../../lib/img/utah.jpg"><p>Utah Churches</p>
-                </div>
+<ul class="nav nav-pills">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="thumbnail">
+                <img src="/lib/img/arizona.jpg">
+                <li role="presentation" class="active"><a href="arizonaHome.php">North West Region(AZ)</a></li>
             </div>
         </div>
-
-<!--Angular Framework-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.5/angular.js"></script>
+        <div class="col-md-4">
+            <div class="thumbnail">
+                <img src="/lib/img/newmex.jpg">
+                <li role="presentation" class="active"><a href="newMexicoHome.php">New Mexico Region(NM)</a></li>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="thumbnail">
+                <img src="/lib/img/utah.jpg">
+                <li role="presentation" class="active"><a href="utahHome.php">Utah Region(UT)</a></li>
+            </div>
+        </div>
+    </div>
+</ul>
+<div class="firstnav">
+    <ul class="nav nav-pills">
+        <li role="presentation" class="active"><a href="clergy.php">Clergy</a></li>
+        <li role="presentation" class="active"><a href="ministries.php">Ministries</a></li>
+        <li role="presentation" class="active"><a href="contact.php">Contact Us</a></li>
+        <li role="presentation" class="active"><a href="upcomingevents.php">Upcoming Events</a></li>
+        <li role="presentation" class="active"><a href="newsletter.php">Newsletter</a></li>
+        <li role="presentation" class="active"><a href="findAChurch.php">Find a Church</a></li>
+    </ul>
+</div>
 </body>
 
 </html>
