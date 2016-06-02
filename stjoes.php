6109a65637af7854da84ee1ab3c8f3fafe21329a
Aarick Lameman <lamemanaarick@gmail.com>
2016-06-02 14:48:14 -0600

    Added stjoes.php

---
+++ stjoes.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php require_once("lib/head-utils.php"); ?>
+    <?php require_once("indexNavBar.php");?>
+    <!-- Custom styles for this template -->
+    <link href="/lib/css/styleindex.css" rel="stylesheet">
+
+    <title>St. Joseph's</title>
+</head>
+<body>
+
+
+<div class="hero-unit" id="bunnies">
+    <div class="row">
+        <div id="logo" class="col-lg-4">
+            <div id="logoimg" class="thumbnail">
+                <img src="/lib/img/epslogo_720.png">
+            </div>
+        </div>
+        <div class="col-lg-2">
+
+
+
+        </div>
+        <div id="bulliten" class="col-lg-6">
+            <h1>ECN's 40th Annual Convocation is Coming Up Soon!</h1>
+            <h3>June 10-12, 2016</h3>
+        </div>
+    </div>
+</div>
+
+
+<div id="bg">
+    <div id="search-container">
+        <div id="search-bg"></div>
+        <div id="search">
+            <div id="bg">
+                <div id="search-container">
+                    <div id="search-bg"></div>
+                    <div id="search">
+
+
+                        <div class="row">
+                            <div class="col-lg-4">
+                                <h3>South East Region</h3>
+                                <h6 span class="state">Arizona Churches</h6></span>
+                                <h6>Good Shepherd Mission</h6>
+                                <h6>St. Josephs Church</h6>
+                                <h6 span class="state">New Mexico Churches</h6></span>
+                                <h6>St. Marks Church</h6>
+                            </div>
+                            <div class="col-lg-4">
+                                <h3>San Juan Region</h3>
+                                <h6 span class="state">New Mexico Churches</h6></span>
+                                <h6>All Saints Church</h6>
+                                <h6>St. Michael's Church</h6>
+                                <h6>St. Luke's In the Desert</h6>
+                            </div>
+                            <div class="col-lg-4">
+                                <h3>Utah Region</h3>
+                                <h6>St. Christopher's Mission</h6>
+                                <h6>St. Mary's of the Moonlight</h6>
+                                <h6>St. John the Baptizer</h6>
+                                <h6>St. David's House</h6>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+    
+
+
+</div>
+</div>
+
+
+
+
+
+<ol class="breadcrumb">
+    <li><a href="index.php">Home</a></li>
+    <li><a href="contact.php">Contact</a></li>
+    <li><a href="currentEvents.php">NewsLetter</a></li>
+    <li><a href="governance.php">Calendar</a></li>
+    <li><a href="support.php">Ministries</a></li>
+    <li class="active">Data</li>
+</ol>
+</body>
+
+</html>
