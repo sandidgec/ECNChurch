73bbc3d0ddefb9059aebabae31faf44637f9a1db
Dana Yazzie <redcloudsaboveus01@gmail.com>
2016-06-02 08:59:49 -0600

    changed navbar/tried to fix

--- contact.php
+++ contact.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <?php require_once("../lib/head-utils.php"); ?>
+    <?php require_once("lib/head-utils.php"); ?>
     <?php require_once("navbar.php");?>
     <title>WireFrame For ECN </title>
     <!-- Custom styles for this template -->
-    <link href="../lib/css/style.css" rel="stylesheet">
+    <link href="/lib/css/style.css" rel="stylesheet">
     <style>
         body {background-color: aqua;}
     </style>
