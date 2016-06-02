516ce27a25083408153590119963dfa05e54e25c
Amber Lee <Lee.amber526@gmail.com>
2016-06-02 10:17:04 -0600

    edited history&beliefs.php

---
+++ history.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php require_once("../lib/head-utils.php"); ?>
+        <?php require_once("navbar.php");?>
+        <title>WireFrame For ECN </title>
+        <!-- Custom styles for this template -->
+        <link href="/lib/css/style.css" rel="stylesheet">
+    </head>
+    <body>
+
+        <div class="jumbotron">
+            <div class="container">
+                <div class="row">
+                    <div class="firstline">
+                        <h2>History & Beliefs </h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    
+
+    </body>
+
+</html>
