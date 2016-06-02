7f16f693ca77d1ae24158d08553da196f1830b7d
Aarick Lameman <lamemanaarick@gmail.com>
2016-06-02 17:42:01 -0600

    Updated the styleindex.css and stjoes.php

--- stjoes.php
+++ stjoes.php
@@ -11,84 +11,84 @@
 <body>
 
 
-<div class="hero-unit" id="bunniesjoe">
-    <div class="row">
-        <div id="logo" class="col-lg-4">
-            <div id="logoimg" class="thumbnail">
-                <img src="/lib/img/episcopalchurchinnavajolandlogo_480.png">
+            <div class="hero-unit" id="bunniesjoe">
+                <h3>Welcome to the St. Joseph's E.C.N. Page</h3>
+
+                <div class="row">
+                    <div id="logo" class="col-lg-4">
+                        <div class="row no-padi">
+                            <img src="/lib/img/episcopalchurchinnavajolandlogo_480.png"
+                                 class="img-responsive"  alt="Awesome.jpg">
+                        </div>
+                    </div>
+
+                    <div id="welcome" class="col-lg-2">
+                    </div>
+
+                    <div id="bulliten" class="col-lg-6">
+                        <h1>ECN's 40th Annual Convocation is Coming Up Soon!</h1>
+                        <h3>June 10-12, 2016</h3>
+                    </div>
+
+                </div>
             </div>
-        </div>
-        <div class="col-lg-2">
 
 
-
-        </div>
-        <div id="bulliten" class="col-lg-6">
-            <h1>ECN's 40th Annual Convocation is Coming Up Soon!</h1>
-            <h3>June 10-12, 2016</h3>
-        </div>
-    </div>
-</div>
-
-
-<div id="bg">
-    <div id="search-container">
-        <div id="search-bg"></div>
-        <div id="search">
             <div id="bg">
                 <div id="search-container">
                     <div id="search-bg"></div>
                     <div id="search">
+                        <div id="bg">
+                            <div id="search-container">
+                                <div id="search-bg"></div>
+                                <div id="search">
 
 
-                        <div class="row">
-                            <div class="col-lg-4">
-                                <h3>Address</h3>
-                                <h6> P.O. Box 618</h6>
-                                <h6>Many Farms, Arizona 86504</h6>
+                                    <div class="row">
+                                        <div class="col-lg-4">
+                                            <h3>Address</h3>
+                                            <h6> P.O. Box 618</h6>
+                                            <h6>Many Farms, Arizona 86504</h6>
 
+                                        </div>
+                                        <div class="col-lg-4">
+                                            <h3>Service Time</h3>
+                                            <h6>9 A.M. - 10 A.M. Every Sunday</h6>
+
+                                        </div>
+                                        <div class="col-lg-4">
+                                            <h3>Programs</h3>
+                                            <h6>Youth Farm Project</h6>
+                                            <h6>June 2, 2016 - September 27, 2016</h6>
+
+                                        </div>
+                                    </div>
+
+                                </div>
                             </div>
-                            <div class="col-lg-4">
-                                <h3>Service Time</h3>
-                                <h6 span class="state">New Mexico Churches</h6></span>
-                                <h6>All Saints Church</h6>
-                                <h6>St. Michael's Church</h6>
-                                <h6>St. Luke's In the Desert</h6>
-                            </div>
-                            <div class="col-lg-4">
-                                <h3>Programs</h3>
-                                <h6>St. Christopher's Mission</h6>
-                                <h6>St. Mary's of the Moonlight</h6>
-                                <h6>St. John the Baptizer</h6>
-                                <h6>St. David's House</h6>
-                            </div>
+
+
                         </div>
-
                     </div>
                 </div>
 
 
+
             </div>
-        </div>
-    </div>
-    
-
-
-</div>
-</div>
+            </div>
 
 
 
 
 
-<ol class="breadcrumb">
-    <li><a href="index.php">Home</a></li>
-    <li><a href="contact.php">Contact</a></li>
-    <li><a href="currentEvents.php">NewsLetter</a></li>
-    <li><a href="governance.php">Calendar</a></li>
-    <li><a href="support.php">Ministries</a></li>
-    <li class="active">Data</li>
-</ol>
+            <ol class="breadcrumb">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="currentEvents.php">NewsLetter</a></li>
+                <li><a href="governance.php">Calendar</a></li>
+                <li><a href="support.php">Ministries</a></li>
+                <li class="active">Data</li>
+            </ol>
 </body>
 
 </html>
