1b5dea82d65f3761a10f203dfa0998ccd55cc560
Aarick Lameman <lamemanaarick@gmail.com>
2016-06-02 13:22:16 -0600

    Updated the adminPanel.php file with tables for each class.

---
+++ adminPanel.php
@@ -0,0 +1,217 @@
+<!DOC TYPE html>
+<html>
+    <head>
+        <?php require_once("lib/head-utils.php"); ?>
+
+        <title>Admin Panel</title>
+
+    </head>
+
+        <body>
+
+        <section class="contact" id="contact">
+            <div class="contact-info">
+
+                <form>
+                    <div class="form-group">
+                        <label for="InputBulletin">Bulletin</label>
+                        <input type="bulletin" class="form-control" id="exampleInputBulletin" placeholder="Bulletin">
+                        <input type="radio" name="" value="">
+                    </div>
+                    <div class="form-group">
+                        <label for="InputMessage">Message</label>
+                        <input type="message" class="form-control" id="exampleInputMessage" placeholder="message">
+                    </div>
+                </form>
+            </div>
+        </section>
+
+            <!-- Tablee for Bulletins -->
+
+            <div class="container">
+
+                        <table class="table table-bordered">
+                            <thead>
+                            <tr>
+                                <th scope="col">Category</th>
+                                <th scope="col">Message</th>
+                                <th scope="col">Time Stamp</th>
+        
+        
+                            </tr>
+                            </thead>
+        
+                            <tbody>
+                            <tr>
+                                <th id="missions">Missions</th>
+                                <th id="message">Nice scenic area of the mission.</th>
+                                <th id="timeStamp">11:55 P.M.</th>
+        
+                            </tr>
+        
+                            </tbody>
+                            <tfoot>
+                            </tfoot>
+        
+                        </table>
+
+            </div>
+
+
+            <!-- Tablee for Members -->
+
+            <div class="container">
+
+                <table class="table table-bordered">
+                    <thead>
+                    <tr>
+                        <th scope="col">First Name</th>
+                        <th scope="col">Last Name</th>
+                        <th scope="col">Position</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">D.O.B.</th>
+                        <th scope="col">Address 1</th>
+                        <th scope="col">Address 2 (Optional)</th>
+                        <th scope="col">City</th>
+                        <th scope="col">State</th>
+                        <th scope="col">Zip Code</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Gender</th>
+                        <th scope="col">Activation</th>
+                        <th scope="col">Salt</th>
+                        <th scope="col">Hash</th>
+
+                    </tr>
+                    </thead>
+
+                    <tbody>
+                    <tr>
+                        <th id="Leon">Leon</th>
+                        <th id="Sampson">Sampson</th>
+                        <th id="PersonInCharge">Person In Charge</th>
+                        <th id="sampsonl74@yahoo.com">SampsonL74@yahoo.com</th>
+                        <th id="December23,1974">December 23, 1974</th>
+                        <th id="3309-E.-Hwy.-162"> 3309 E. Hwy. 162 </th>
+                        <th id="House #2">House #2</th>
+                        <th id="Bluff">Bluff</th>
+                        <th id="Utah">Utah</th>
+                        <th id="84512">84512</th>
+                        <th id="435-485-0727">435-672-0727</th>
+                        <th id="Male">Male</th>
+                        <th id="Activation">Activation</th>
+                        <th id="Salt">Salt</th>
+                        <th id="Hash">Hash</th>
+                    </tr>
+
+                    </tbody>
+                    <tfoot>
+                    </tfoot>
+
+                </table>
+
+
+
+        </div>
+
+            <!-- Tablee for Missions -->
+
+            <div class="container">
+
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th scope="col">Name</th>
+                            <th scope="col">Address 1</th>
+                            <th scope="col">Address 2 (Optional)</th>
+                            <th scope="col">City</th>
+                            <th scope="col">State</th>
+                            <th scope="col">Zip Code</th>
+                            <th scope="col">Phone</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Person In Charge</th>
+                            <th scope="col">Service Time</th>
+
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <th id="St.-Christopher's">St. Christopher's</th>
+                            <th id="3309-E.-Hwy.-162">3309 E. Hwy. 162</th>
+                            <th id="Church">Church</th>
+                            <th id="Bluff">Bluff</th>
+                            <th id="Utah">Utah</th>
+                            <th id="84512">84512</th>
+                            <th id="435-672-2206s">435-672-2206</th>
+                            <th id="sampsonl74@yahoo.com">SampsonL74@yahoo.com</th>
+                            <th id="Leon-Sampson">Leon Sampson</th>
+                            <th id="9am">9 A.M.-10 A.M. Every Sunday</th>
+
+                        </tr>
+                    </tbody>
+                    <tfoot>
+                    </tfoot>
+                </table>
+
+
+
+        </div>
+
+
+
+        <!-- Table for Programs -->
+
+        <div class="container">
+
+            <table class="table table-bordered">
+                <thead>
+                <tr>
+                    <th scope="col">Program Name</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Location</th>
+                    <th scope="col">Program Time</th>
+                    <th scope="col">Program Date</th>
+
+                </tr>
+                </thead>
+
+                <tbody>
+                <tr>
+                    <th id="BlueCornProject">Blue Corn Project</th>
+                    <th id="description">UTO Grant from 2012 Geeneral Convention allowed ECN with start-up funds to grow,
+                        mill, and produce product to create Economic Development opertunities to help out reach programs
+                        with in communities.</th>
+                    <th id="Bluff">Bluff, Utah Homer Dale Community Farm</th>
+                    <th id="summer">Summer Days</th>
+                    <th id="May-Sept">May to September</th>
+
+                </tr>
+
+                </tbody>
+                <tfoot>
+                </tfoot>
+
+            </table>
+
+
+
+        </div>
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </body>
+</html>
---
+++ bluecornproject.php
@@ -0,0 +1,12 @@
+<!DOC TYPE html>
+<html>
+    <head>
+        <title>Blue Corn Project</title>
+    </head>
+
+        <body>
+            <p>UTO Grant from 2012 Geeneral Convention allowed ECN with start-up funds to grow,
+                mill, and produce product to create Economic Development opertunities to help out reach programs
+                with in communities</p>
+        </body>
+</html>
