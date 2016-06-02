28b4915b5b70196ecdf40bb02a2e791e3366401b
Amber Lee <Lee.amber526@gmail.com>
2016-06-02 10:18:33 -0600

    changed file name

--- history.php
+++ history.php
@@ -1,26 +1,25 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <?php require_once("../lib/head-utils.php"); ?>
-        <?php require_once("navbar.php");?>
-        <title>WireFrame For ECN </title>
-        <!-- Custom styles for this template -->
-        <link href="/lib/css/style.css" rel="stylesheet">
-    </head>
-    <body>
+<head>
+    <?php require_once("../lib/head-utils.php"); ?>
+    <?php require_once("navbar.php"); ?>
+    <title>WireFrame For ECN </title>
+    <!-- Custom styles for this template -->
+    <link href="/lib/css/style.css" rel="stylesheet">
+</head>
+<body>
 
-        <div class="jumbotron">
-            <div class="container">
-                <div class="row">
-                    <div class="firstline">
-                        <h2>History & Beliefs </h2>
-                    </div>
-                </div>
+<div class="jumbotron">
+    <div class="container">
+        <div class="row">
+            <div class="firstline">
+                <h2>History & Beliefs </h2>
             </div>
         </div>
+    </div>
+</div>
 
     
-
-    </body>
+</body>
 
 </html>
