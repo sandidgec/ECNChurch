3efec44fd928fab9f18c0afb40306e8ea8903080
Ryan Sam <rnsam@asu.edu>
2016-06-14 12:00:01 -0600

    added content to allSaints.php

--- allSaints.php
+++ allSaints.php
@@ -9,6 +9,77 @@
 </head>
 
 <body>
-<p>New Mexico Region Site</p>
+<h1>All Saints</h1>
+<p>Farmington New Mexico</p>
+
+<div class="hero-unit" id="allsaints">
+    <h3>Welcome to the All Saints E.C.N.</h3>
+
+    <div class="row">
+        <div id="logo" class="col-lg-4">
+            <div class="row no-padi">
+                <img src="/lib/img/episcopalchurchinnavajolandlogo_480.png"
+                     class="img-responsive"  alt="Awesome.jpg">
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="bg">
+    <div id="search-container">
+        <div id="search-bg"></div>
+        <div id="search">
+            <div id="bg">
+                <div id="search-container">
+                    <div id="search-bg"></div>
+                    <div id="search">
+
+
+                        <div class="row">
+                            <div class="col-lg-4">
+                                <h3>Address</h3>
+                                <h6>1271 Mission Ave</h6>
+                                <h6>Phone:(505) 327-0326</h6>
+                                <h6>Farmington, NM 87401</h6>
+
+                            </div>
+                            <div class="col-lg-4">
+                                <h3>Service Time</h3>
+                                <h6>10 A.M. - 12 P.M. Every Sunday</h6>
+
+                            </div>
+                            <div class="col-lg-4">
+                                <h3>Programs</h3>
+                                <h6>VBS</h6>
+                                <h6>2016</h6>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+
+
+
+</div>
+</div>
+
+
+
+
+
+<ol class="breadcrumb">
+    <li><a href="index.php">Home</a></li>
+    <li><a href="contact.php">Contact</a></li>
+    <li><a href="currentEvents.php">NewsLetter</a></li>
+    <li><a href="governance.php">Calendar</a></li>
+    <li><a href="support.php">Ministries</a></li>
+    <li class="active">Data</li>
+</ol>
 </body>
 </html>
