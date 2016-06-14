910f7caf7c1b5c7343973f95a114280878989219
Gjgordy <gjgordy1996@yahoo.com>
2016-06-14 10:43:14 -0600

    added st marks, stmarkscss, stmarys, allsaintscss and requireonce info.

---
+++ stMarks.php
@@ -0,0 +1,14 @@
+<!DOC TYPE html>
+<html>
+<head>
+    <?php require_once("lib/head-utils.php"); ?>
+    <?php require_once("indexNavBar.php");?>
+    <!-- Custom styles for this template -->
+    <link href="/lib/css/stMarks.css" rel="stylesheet">
+    <title>St. Marks</title>
+</head>
+
+<body>
+<p>Utah Region Site</p>
+</body>
+</html>
--- stMarys.php
+++ stMarys.php
@@ -4,7 +4,7 @@
         <?php require_once("lib/head-utils.php"); ?>
         <?php require_once("indexNavBar.php");?>
         <!-- Custom styles for this template -->
-        <link href="/lib/css/stmarys.css" rel="stylesheet">
+        <link href="/lib/css/stMarys.css" rel="stylesheet">
     <title>Utah</title>
 </head>
 
