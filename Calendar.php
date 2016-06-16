c9ee1ee50ce446046bf0dba47535e7fa9f69a036
Gjgordy <gjgordy1996@yahoo.com>
2016-06-16 09:43:38 -0600

    added region pages and region css.

--- Calendar.php
+++ Calendar.php
@@ -3,22 +3,218 @@
 <head>
     <?php require_once("lib/head-utils.php"); ?>
     <?php require_once("indexNavBar.php");?>
-    <title>WireFrame For ECN </title>
+    <title>Calendar</title>
     <!-- Custom styles for this template -->
-    <link href="/lib/css/style.css" rel="stylesheet">
+    <link href="/lib/css/stylecalendar.css" rel="stylesheet">
 </head>
+    <title>June 2016</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 <body>
+    <div id="calendar-wrap">
+        <header>
+            <h1>June 2016</h1>
+        </header>
+        <div id="calendar">
+            <ul class="weekdays">
+                <li>Sunday</li>
+                <li>Monday</li>
+                <li>Tuesday</li>
+                <li>Wednesday</li>
+                <li>Thursday</li>
+                <li>Friday</li>
+                <li>Saturday</li>
+            </ul>
+            <ul class="days">
+                <li class="day other-month">
+                    <div class="date">29</div>
+                </li>
+                <li class="day other-month">
+                    <div class="date">30</div>
+                </li>
+                <li class="day other-month">
+                    <div class="date">31</div>
+                </li>
+                <li class="day">
+                    <div class="date">1</div>
+                </li>
+                <li class="day">
+                    <div class="date">2</div>
+                </li>
+                <li class="day">
+                    <div class="date">3</div>
+                    <div class="event">
+                        <div class="enent-desc">
+                            Cohort 1 Graduation Ceramony
+                        </div>
+                        <div class="event-time">
+                            11:30am-2:30pm
+                        </div>
+                    </div>
+                </li>
+                <li class="day">
+                    <div class="date">4</div>
+                </li>
+            </ul>
+            <ul class="days">
+                <li class="day">
+                    <div class="date">5</div>
+                </li>
+                <li class="day">
+                    <div class="date">6</div>
+                </li>
+                <li class="day">
+                    <div class="date">7</div>
+                </li>
+                <li class="day">
+                    <div class="date">8</div>
+                </li>
+                <li class="day">
+                    <div class="date">9</div>
+                </li>
+                <li class="day">
+                    <div class="date">10</div>
+                    <div class="event">
+                        <div class="enent-desc">
+                           40th Convocation of the Episcopal Church in Navajoland-Registration
+                        </div>
+                        <div class="event-time">
+                            4:00pm-7:30pm
+                        </div>
+                    </div>
+                </li>
+                <li class="day">
+                    <div class="date">11</div>
+                    <div class="event">
+                        <div class="enent-desc">
+                            40th Convocation of the Episcopal Church in Navajoland-Lectures
+                        </div>
+                        <div class="event-time">
+                            7:30pm-8:30pm
+                        </div>
+                    </div>
+                </li>
+            </ul>
+            <ul class="days">
+                <li class="day">
+                    <div class="date">12</div>
+                    <div class="event">
+                        <div class="enent-desc">
+                            40th Convocation of the Episcopal Church in Navajoland-Business
+                        </div>
+                        <div class="event-time">
+                            7:30pm-1:30pm
+                        </div>
+                    </div>
+                </li>
+                <li class="day">
+                    <div class="date">13</div>
+                    <div class="event">
+                        <div class="enent-desc">
+                            All Saints VBS
+                        </div>
+                        <div class="event-time">
+                            9:00pm-12:00pm
+                        </div>
+                    </div>
+                </li>
+                <li class="day">
+                    <div class="date">14</div>
+                    <div class="event">
+                        <div class="enent-desc">
+                            All Saints VBS
+                        </div>
+                        <div class="event-time">
+                            9:00pm-12:00pm
+                        </div>
+                    </div>
+                </li>
+                <li class="day">
+                    <div class="date">15</div>
+                    <div class="event">
+                        <div class="enent-desc">
+                            All Saints VBS
+                        </div>
+                        <div class="event-time">
+                            9:00pm-12:00pm
+                        </div>
+                    </div>
+                </li>
+                <li class="day">
+                    <div class="date">16</div>
+                    <div class="event">
+                        <div class="enent-desc">
+                            All Saints VBS
+                        </div>
+                        <div class="event-time">
+                            9:00pm-12:00pm
+                        </div>
+                    </div>
+                </li>
+                <li class="day">
+                    <div class="date">17</div>
+                </li>
+                <li class="day">
+                    <div class="date">18</div>
+                </li>
+            </ul>
+            <ul class="days">
+                <li class="day">
+                    <div class="date">19</div>
+                </li>
+                <li class="day">
+                    <div class="date">20</div>
+                </li>
+                <li class="day">
+                    <div class="date">21</div>
+                </li>
+                <li class="day">
+                    <div class="date">22</div>
+                </li>
+                <li class="day">
+                    <div class="date">23</div>
+                    <div class="event">
+                        <div class="enent-desc">
+                            Cohort 1 Meeting with Cultivating Coders &ECN
+                        </div>
+                        <div class="event-time">
+                            4:30pm-6:30pm
+                        </div>
+                    </div>
+                </li>
+                <li class="day">
+                    <div class="date">24</div>
+                </li>
+                <li class="day">
+                    <div class="date">25</div>
+                </li>
+            </ul>
+            <ul class="days">
+                <li class="day">
+                    <div class="date">26</div>
+                </li>
+                <li class="day">
+                    <div class="date">27</div>
+                </li>
+                <li class="day">
+                    <div class="date">28</div>
+                </li>
+                <li class="day">
+                    <div class="date">29</div>
+                </li>
+                <li class="day">
+                    <div class="date">30</div>
+                </li>
+                <li class="day other-month">
+                    <div class="date">1</div>
+                </li>
+                <li class="day other-month">
+                    <div class="date">2</div>
+                </li>
+            </ul>
+        </div><!--/. calendar -->
+    </div> <!--/. wrap -->
 
 
-<div class="jumbotron">
-    <div class="container">
-        <div class="row">
-            <div class="firstline">
-                <h2>Find your current events here. </h2>
-            </div>
-        </div>
-    </div>
-</div>
 </body>
 
 </html>
