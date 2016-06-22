8a6230b4cae009f0b30d9a77923c55d1f47cc977
marietab <mbuck12231989@gmail.com>
2016-06-22 09:43:37 -0600

    added info in st. Marys

--- stMarys.php
+++ stMarys.php
@@ -5,20 +5,31 @@
         <?php require_once("indexNavBar.php");?>
         <!-- Custom styles for this template -->
         <link href="/lib/css/stMarys.css" rel="stylesheet">
-    <title>Utah</title>
+    <title></title>
 </head>
 
 <body>
-<div class="jumbotron">
+<div class="jumbotron1">
     <h1>Saint Mary's in the Moonlight</h1>
-    <p>...</p>
-    <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+    <p1>Welcome to St. Marys, ECN.</p1>
 </div>
 
-<div class="jumbotron">
-    <h1>Hello, world!</h1>
-    <p>...</p>
-    <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+<div class="jumbotron2">
+    <h1></h1>
+    <div class="row">
+        <div class="span4">
+           <h2>Address</h2>
+            <p2>P.O. Box 3603 Oljato, UT 84534</p2>
+        </div>
+        <div class="span4">
+            <h3>Service Time</h3>
+            <p3></p3>
+        </div>
+        <div class="span4">
+            <h4>Programs</h4>
+            <p4></p4>
+        </div>
+    </div>
 </div>
 
 </body>
---
+++ utahRegion.php
@@ -0,0 +1,127 @@
+<!DOC TYPE html>
+<html>
+<head>
+    <?php require_once("lib/head-utils.php"); ?>
+    <?php require_once("indexNavBar.php");?>
+    <!-- Custom styles for this template -->
+    <link href="/lib/css/styleUtah.css" rel="stylesheet">
+    <title>UTAH Region</title>
+</head>
+<body>
+
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+        <div class="item active">
+            <img src="/lib/img/stChristopherSign.jpg" alt="...">
+            <div class="carousel-caption">
+                ...
+            </div>
+        </div>
+        <div class="item">
+            <img src="/lib/img/stChristopherInterior.jpg" alt="...">
+            <div class="carousel-caption">
+                ...
+            </div>
+        </div>
+        <div class="item">
+            <img src="/lib/img/bellAndHogan.jpg" alt="...">
+            <div class="carousel-caption">
+                ...
+            </div>
+        </div>
+        <div class="item">
+            <img src="/lib/img/stMaryInterior.jpg" alt="...">
+            <div class="carousel-caption">
+                ...
+            </div>
+        </div>
+        <div class="item">
+            <img src="/lib/img/stJohnSign.jpg" alt="...">
+            <div class="carousel-caption">
+                ...
+            </div>
+        </div>
+        ...
+    </div>
+
+    <!-- Controls -->
+    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+
+<div class="row logo-container">
+    <div id="logo" class="col-lg-4">
+        <div class="row no-pad">
+            <img src="/lib/img/episcopalchurchinnavajolandlogo_480.png"
+                 class="img-responsive"  alt="Awesome.jpg">
+        </div>
+    </div>
+</div>
+
+<div class="jumbotron">
+    <div class="container">
+        <h1>Utah Region</h1>
+        <p>Utah region consist of three churches</p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-6">
+        <h2>The Rt. Rev. David Bailey</h2>
+        <h3>Cell:(801) 891-2418</h3>
+        <h3>Email: ecnbailey@gmail.com</h3>
+    </div>
+    <div class="col-lg-6">
+        <h2>Rev. Chan Anaya</h2>
+        <h3>Cell:(505)716-8445</h3>
+        <h3>Email: ecnanaya@gmail.com</h3>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-4">
+        <h2>St. Christopher Mission</h2>
+        <h3>Leon Sampson, Deacon in Charge</h3>
+        <h3>Cell:(435) 444-0485</h3>
+        <h3>sampson174@yahoo.com</h3>
+        <h3>P.O. Box 28 Bluff</h3>
+        <h3>Utah, 84512</h3>
+    </div>
+    <div class="col-lg-4">
+        <h2>St. Mary's of the Moonlight</h2>
+        <h3>Catherine Plummer, Priest in Charge</h3>
+        <h3>Office:(435) 727-0200</h3>
+        <h3>Cell:(435) 979-5611</h3>
+        <h3>P.O. Box 360397 Oljato</h3>
+        <h3>Utah, 84536</h3>
+    </div>
+    <div class="col-lg-4">
+        <h2>St. John the Baptizer</h2>
+        <h3>Lillie Henderson, Lay Pastor/Children's Ministry Director</h3>
+        <h3>Office:(435) 651-3225</h3>
+        <h3>Cell:(435)444-0484</h3>
+        <h3>P.O. Box Montezuma Creek</h3>
+        <h3>Utah, 84534</h3>
+    </div>
+</div>
+
+
+
+</div>
+</div>
+</body>
+</html>
