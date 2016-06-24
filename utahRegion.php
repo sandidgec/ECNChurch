e7cd5e98cd043c85d303b876368c154b4f08503e
marietab <mbuck12231989@gmail.com>
2016-06-24 15:58:08 -0600

    added utahRegion data

--- utahRegion.php
+++ utahRegion.php
@@ -17,7 +17,6 @@
         <li data-target="#carousel-example-generic" data-slide-to="1"></li>
         <li data-target="#carousel-example-generic" data-slide-to="2"></li>
         <li data-target="#carousel-example-generic" data-slide-to="3"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="4"></li>
     </ol>
 
     <!-- Wrapper for slides -->
@@ -35,19 +34,13 @@
             </div>
         </div>
         <div class="item">
-            <img src="/lib/img/bellAndHogan.jpg" alt="...">
-            <div class="carousel-caption">
-                ...
-            </div>
-        </div>
-        <div class="item">
             <img src="/lib/img/stMaryInterior.jpg" alt="...">
             <div class="carousel-caption">
                 ...
             </div>
         </div>
         <div class="item">
-            <img src="/lib/img/stJohnSign.jpg" alt="...">
+            <img src="/lib/img/stJohnsinterior.jpg" alt="...">
             <div class="carousel-caption">
                 ...
             </div>
@@ -66,57 +59,79 @@
     </a>
 </div>
 
-<div class="row logo-container">
-    <div id="logo" class="col-lg-4">
-        <div class="row no-pad">
-            <img src="/lib/img/episcopalchurchinnavajolandlogo_480.png"
-                 class="img-responsive"  alt="Awesome.jpg">
+
+<div class="jumbotron">
+    <div class="row logo-container">
+        <div id="logo" class="col-lg-5">
+            <div class="row no-pad">
+                <img src="/lib/img/episcopalchurchinnavajolandlogo_480.png"
+                     class="img-responsive"  alt="Awesome.jpg">
+            </div>
+        </div>
+        <div class="col-lg-7">
+            <h1>Welcome to the Utah Region</h1>
+        </div>
+
+    </div>
+
+</div>
+<div class="jumbotron">
+    <div class="row utahMission-container">
+        <div class="col-lg-6">
+            <h2>The Rt. Rev. David Bailey</h2>
+            <h3>Bishop of Navajoland</h3>
+            <h3>Email: ecnbailey@gmail.com</h3>
+        </div>
+        <div class="col-lg-6">
+            <h2>Rev. Chan Anaya</h2>
+            <h3>Priest in Charge St. Michael's Canon Missioner</h3>
+            <h3>Email: ecnanaya@gmail.com</h3>
         </div>
     </div>
 </div>
-
 <div class="jumbotron">
-    <div class="container">
-        <h1>Utah Region</h1>
-        <p>Utah region consist of three churches</p>
+    <div class="row utahMission-container">
+        <div class="col-lg-4">
+            <h2>Leon Sampson</h2>
+            <h3>Deacon in Charge at St. Christopher's</h3>
+            <h3>Cell:(435) 444-0485</h3>
+            <h3>sampson174@yahoo.com</h3>
+        </div>
+        <div class="col-lg-4">
+            <h2>Catherine Plummer</h2>
+            <h3>Priest in Charge at St. Mary's</h3>
+            <h3>Cell:(435) 979-5611</h3>
+        </div>
+        <div class="col-lg-4">
+            <h2>Lillie Henderson</h2>
+            <h3>Lay Pastor/Children's Ministry Director at St. John's</h3>
+            <h3>Cell:(435)444-0484</h3>
+        </div>
     </div>
 </div>
-<div class="row">
-    <div class="col-lg-6">
-        <h2>The Rt. Rev. David Bailey</h2>
-        <h3>Cell:(801) 891-2418</h3>
-        <h3>Email: ecnbailey@gmail.com</h3>
-    </div>
-    <div class="col-lg-6">
-        <h2>Rev. Chan Anaya</h2>
-        <h3>Cell:(505)716-8445</h3>
-        <h3>Email: ecnanaya@gmail.com</h3>
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-4">
-        <h2>St. Christopher Mission</h2>
-        <h3>Leon Sampson, Deacon in Charge</h3>
-        <h3>Cell:(435) 444-0485</h3>
-        <h3>sampson174@yahoo.com</h3>
-        <h3>P.O. Box 28 Bluff</h3>
-        <h3>Utah, 84512</h3>
-    </div>
-    <div class="col-lg-4">
-        <h2>St. Mary's of the Moonlight</h2>
-        <h3>Catherine Plummer, Priest in Charge</h3>
-        <h3>Office:(435) 727-0200</h3>
-        <h3>Cell:(435) 979-5611</h3>
-        <h3>P.O. Box 360397 Oljato</h3>
-        <h3>Utah, 84536</h3>
-    </div>
-    <div class="col-lg-4">
-        <h2>St. John the Baptizer</h2>
-        <h3>Lillie Henderson, Lay Pastor/Children's Ministry Director</h3>
-        <h3>Office:(435) 651-3225</h3>
-        <h3>Cell:(435)444-0484</h3>
-        <h3>Montezuma Creek</h3>
-        <h3>Utah, 84534</h3>
+<div class="jumbotron2">
+    <div class="row utahMission-container">
+        <div class="col-lg-4">
+            <h2>St. Christopher's Mission</h2>
+            <h3>Office:(435)-672-2206 </h3>
+            <h3>P.O. Box 28 Bluff</h3>
+            <h3>Utah, 84512</h3>
+            <a href="http://www.facebook.com"> <i class="fa fa-facebook-official fa-2x"></i></a>
+        </div>
+        <div class="col-lg-4">
+            <h2>St. Mary's of the Moonlight</h2>
+            <h3>Office:(435) 727-0200</h3>
+            <h3>P.O. Box 360397 Oljato</h3>
+            <h3>Utah, 84536</h3>
+            <a href="http://www.facebook.com"> <i class="fa fa-facebook-official fa-2x"></i></a>
+        </div>
+        <div class="col-lg-4">
+            <h2>St. John the Baptizer</h2>
+            <h3>Office:(435) 651-3225</h3>
+            <h3>Montezuma Creek</h3>
+            <h3>Utah, 84534</h3>
+            <a href="http://www.facebook.com"> <i class="fa fa-facebook-official fa-2x"></i></a>
+        </div>
     </div>
 </div>
 
