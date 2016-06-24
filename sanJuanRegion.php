0bc48f4902ff6aee34846eb6ad16ab2c746f5312
Ryan Sam <rnsam@asu.edu>
2016-06-24 15:53:57 -0600

    added carousel to southeast and san juan region

--- sanJuanRegion.php
+++ sanJuanRegion.php
@@ -10,22 +10,52 @@
 
 <body>
 
-<div class="hero-unit">
-    <div class="container col-md-6" id="container">
-        <h1>San Juan Region </h1>
-         <p>New Mexico Missions</p>
-        <a href="#learn-more" role="button" class="btn btn-info btn-large">Learn more</a>
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+        <div class="item active">
+            <img src="/lib/img/allsaint.jpg" alt="...">
+            <div class="carousel-caption">
+                <div class="container col-md-7" id="test">
+                    <h1>San Juan Region </h1>
+                    <p>New Mexico Churches</p>
+                    <a href="#learn-more" role="button" class="btn btn-info btn-large">Learn more</a>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img src="/lib/img/stMichaels1.jpg" alt="...">
+            <div class="carousel-caption">
+                ...
+            </div>
+        </div>
+        ...
     </div>
+
+    <!-- Controls -->
+    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
 </div>
-
-
 
 <div class="text-center" id="title">
     <h1>Welcome to the San Juan Region</h1>
 </div>
 
 <div class="text-center" id="info">
-    <p>New Mexico Missions</p>
+    <p>New Mexico Churches</p>
 </div>
 
 <div class="row" id="learn-more">
@@ -98,5 +128,6 @@
     <li><a href="support.php">Ministries</a></li>
     <li class="active">Data</li>
 </ol>
+
 </body>
 </html>
---
+++ southEast.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php require_once("lib/head-utils.php"); ?>
+    <?php require_once("indexNavBar.php");?>
+    <!-- Custom styles for this template -->
+    <link href="lib/css/stylesSouthEast.css" rel="stylesheet">
+    <title>South East Region</title>
+</head>
+
+<body>
+
+
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+    </ol>
+
+    
+    
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+        <div class="item active">
+            <img src="/lib/img/gsm1.jpg" alt="...">
+            <div class="carousel-caption">
+                <div class="container col-md-11" id="test">
+                    <h1>South East Region </h1>
+                    <p>Arizona and New Mexico Churches</p>
+                    <a href="#learn-more" role="button" class="btn btn-info btn-large">Learn more</a>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img src="/lib/img/GSMBell.JPG" alt="...">
+            <div class="carousel-caption">
+                ...
+            </div>
+        </div>
+        <div class="item">
+            <img src="/lib/img/stMarks.jpg" alt="...">
+            <div class="carousel-caption">
+                ...
+            </div>
+        </div>
+        ...
+    </div>
+
+    <!-- Controls -->
+    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+
+
+
+<div class="text-center" id="title">
+    <h1>Welcome to the South East Region</h1>
+</div>
+
+<div class="text-center" id="info">
+    <p>Arizona and New Mexico Churches</p>
+</div>
+
+<div class="row" id="learn-more">
+
+    <div class="col-lg-4">
+        <h3>St. Josephs Church</h3>
+        <h6>Many Farms, Arizona</h6>
+        <h6>P.O. Box 618</h6>
+        <h6>Fort Defiance, AZ 86504</h6>
+            <a href="http://www.facebook.com"> <i class="fa fa-facebook-official fa-2x"></i></a>
+
+        <dl>
+            <dt>Rev. Deacon Paula Henson</dt>
+            <dd>Office No. (928) 729-2322</dd>
+            <dd>E-Mail. officegoodshepherdmission@gmail.com</dd>
+        </dl>
+
+    </div>
+
+
+    <div class="col-lg-4">
+        <h3>Good Shepherd Mission</h3>
+        <h6>1 Kit Carson Drive</h6>
+        <h6>Phone:(928) 729-2322</h6>
+        <h6>P.O. Box 618</h6>
+        <h6>Fort Defiance, AZ 86504</h6>
+
+
+                <a href="http://www.facebook.com"> <i class="fa fa-facebook-official fa-2x"></i></a>
+
+
+        <dl id="space">
+            <dt>Rev. Mother Cynthia Hizer</dt>
+            <dd>Office No. (928) 729-2322</dd>
+            <dd>Email: vicargoodshepherdmission@gmail.com</dd>
+            <dd>Priest In Charge Good Shepherd Mission</dd>
+            <br>
+            <dt>Rev. Deacon Paula Henson </dt>
+            <dd>Office No. (928) 729-2322</dd>
+            <dd>E-Mail. officegoodshepherdmission@gmail.com</dd>
+            <dd>Mission Coordinator</dd>
+            <br>
+            <a href="http://www.goodshepmission.org" type="button" class="btn btn-primary">Good Shepherd Mission</a>
+
+        </dl>
+
+    </div>
+
+
+
+
+
+    <div class="col-lg-4">
+        <h3>St. Marks Church</h3>
+        <h6>Coalmine, New Mexico</h6>
+        <h6>Office No. (928) 729-2322</h6>
+        <h6>P.O. Box 618</h6>
+        <h6>Fort Defiance, AZ 86504</h6>
+        <a href="http://www.facebook.com"> <i class="fa fa-facebook-official fa-2x"></i></a>
+
+        <dl>
+            <dt>Rev. Mother Cynthia Hizer</dt>
+            <dd>Office No. (928) 729-2322</dd>
+            <dd>P.O. Box 618</dd>
+            <dd>Fort Defiance, AZ 86504</dd>
+            <dd>Priest In Charge at St. Mark's</dd>
+        </dl>
+
+    </div>
+</div>
+
+<ol class="breadcrumb">
+    <li><a href="index.php">Home</a></li>
+    <li><a href="contact.php">Contact</a></li>
+    <li><a href="currentEvents.php">NewsLetter</a></li>
+    <li><a href="governance.php">Calendar</a></li>
+    <li><a href="support.php">Ministries</a></li>
+    <li class="active">Data</li>
+</ol>
+</body>
+</html>
