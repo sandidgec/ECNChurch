07fe5478f63e2466f29529acb669bbaed8cf343d
Ryan Sam <rnsam@asu.edu>
2016-06-27 16:50:55 -0600

    added maps to find a church.php

--- findaChurch.php
+++ findaChurch.php
@@ -42,7 +42,7 @@
                 </div>
 
                 <div class="container find" id="find">
-                    <h3>San Juan Region <a href="allSaints.php">Page</a></h3>
+                    <h3>San Juan Region <a href="sanJuanRegion.php">Page</a></h3>
                     <div class="row">
 
                         <div class="col-lg-3">
@@ -65,15 +65,15 @@
                         </div>
 
                         <div class="col-lg-3">
-                            <h4>Info/<a href="allSaints.php">Map</a></h4>
-                            <h4>Info/<a href="allSaints.php">Map</a></h4>
-                            <h4>Info/<a href="allSaints.php">Map</a></h4>
+                            <h4>Info/<a href="#SJ">Map</a></h4>
+                            <h4>Info/<a href="#SJ">Map</a></h4>
+                            <h4>Info/<a href="#SJ">Map</a></h4>
                         </div>
                     </div>
                 </div>
 
                 <div class="container find" id="find">
-                    <h3>Southeast Region <a href="allSaints.php">Page</a></h3>
+                    <h3>Southeast Region <a href="southEast.php">Page</a></h3>
                     <div class="row">
 
                         <div class="col-lg-3">
@@ -96,15 +96,15 @@
                         </div>
 
                         <div class="col-lg-3">
-                            <h4>Info/<a href="allSaints.php">Map</a></h4>
-                            <h4>Info/<a href="allSaints.php">Map</a></h4>
-                            <h4>Info/<a href="allSaints.php">Map</a></h4>
+                            <h4>Info/<a href="#SE">Map</a></h4>
+                            <h4>Info/<a href="#SE">Map</a></h4>
+                            <h4>Info/<a href="#SE">Map</a></h4>
                         </div>
                     </div>
                 </div>
 
                 <div class="container find" id="find">
-                    <h3>Utah Region <a href="allSaints.php">Page</a></h3>
+                    <h3>Utah Region <a href="utahRegion.php">Page</a></h3>
                     <div class="row">
 
                         <div class="col-lg-3">
@@ -127,14 +127,36 @@
                         </div>
 
                         <div class="col-lg-3">
-                            <h4>Info/<a href="allSaints.php">Map</a></h4>
-                            <h4>Info/<a href="allSaints.php">Map</a></h4>
-                            <h4>Info/<a href="allSaints.php">Map</a></h4>
+                            <h4>Info/<a href="#UR">Map</a></h4>
+                            <h4>Info/<a href="#UR">Map</a></h4>
+                            <h4>Info/<a href="#UR">Map</a></h4>
                         </div>
                     </div>
                 </div>
         </div>
     </div>
+</div>
+
+<div class="row" id="SJ">
+    <h1>San Juan Region</h1>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3589.9551047239083!2d-108.2272004351544!3d36.715681778026834!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xdc8a20da2bbcb726!2sEpiscopal+Church+In+Navajoland!5e0!3m2!1sen!2sus!4v1467045233025" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3198.2914186490702!2d-108.34643338518232!3d36.7155617801601!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x873b9750066acd2b%3A0xc7b5999b8195b9be!2sSaint+Michael+Church!5e0!3m2!1sen!2sus!4v1467045413601" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30566.225994060253!2d-105.77465795953154!3d36.36371612259727!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8717788ebb67b883%3A0x646f52ad0fe43b4d!2sCarson%2C+NM+87517!5e0!3m2!1sen!2sus!4v1467049672874" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+</div>
+
+<div class="row" id="SE">
+    <h1>South East Region</h1>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3140.2087554742916!2d-109.0705667600941!3d35.74249031533021!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87253349d1249e79%3A0x1c47bb254486d3b7!2sGood+Shepherd+Mission!5e0!3m2!1sen!2sus!4v1467047644763" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3853.2138984367316!2d-108.96882182169969!3d35.69783381028325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x6f7abcd506f57d4a!2sSt.+Mark&#39;s+Episcopal+Church+of+Navajoland!5e0!3m2!1sen!2sus!4v1467047742685" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d51414.902145279295!2d-109.66428114589186!3d36.3503285710563!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x873080f0cd87ee01%3A0xf1b083280e0855aa!2sMany+Farms%2C+AZ!5e0!3m2!1sen!2sus!4v1467049714293" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
+
+<div class="row" id="UR">
+    <h1>Utah Region</h1>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5338.6619800449935!2d-109.51740519824793!3d37.28595123508494!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8739e10ebfcc5c2f%3A0x31dcc937d45b1a12!2sSaint+Christophers+Mission!5e0!3m2!1sen!2sus!4v1467047931897" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3186.4337519059122!2d-110.27835181295775!3d36.999447289934366!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87372903d6882cef%3A0xdbd64e35aebf7057!2sHogan+Church!5e0!3m2!1sen!2sus!4v1467055865183" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d50807.75162614793!2d-109.34118708885372!3d37.259937106758976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8739eef185f006e5%3A0x90d170622c5a2f4a!2sMontezuma+Creek%2C+UT+84534!5e0!3m2!1sen!2sus!4v1467049618274" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
 </div>
 
 
