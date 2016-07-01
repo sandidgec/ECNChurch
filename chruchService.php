f7423379e2829756cee4180f33f5c11611d465e8
Gjgordy <gjgordy1996@yahoo.com>
2016-07-01 07:32:26 -0600

    added calendar stuff.

---
+++ chruchService.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php require_once("lib/head-utils.php"); ?>
+    <?php require_once("indexNavBar.php");?>
+    <title>Services/Events</title>
+    <!-- Custom styles for this template -->
+</head>
+
+<body>
+
+
+
+
+
+</body>
+
+
+</html>
