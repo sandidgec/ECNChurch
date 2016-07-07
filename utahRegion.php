c1495e7bdc1990c1eb0c15511865a5c9eba374bb
marietab <mbuck12231989@gmail.com>
2016-07-07 16:51:10 -0600

    added info and pictures to utah region

--- utahRegion.php
+++ utahRegion.php
@@ -1,14 +1,14 @@
-<!DOC TYPE html>
+<!DOCTYPE html>
 <html>
 <head>
     <?php require_once("lib/head-utils.php"); ?>
     <?php require_once("indexNavBar.php");?>
     <!-- Custom styles for this template -->
-    <link href="/lib/css/styleUtah.css" rel="stylesheet">
-    <title>UTAH Region</title>
+    <link href="lib/css/styleSanJuan.css" rel="stylesheet">
+    <title>San Juan Region</title>
 </head>
-<body>
 
+<body>
 
 <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
     <!-- Indicators -->
@@ -16,7 +16,6 @@
         <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
         <li data-target="#carousel-example-generic" data-slide-to="1"></li>
         <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="3"></li>
     </ol>
 
     <!-- Wrapper for slides -->
@@ -47,7 +46,6 @@
         </div>
         ...
     </div>
-
     <!-- Controls -->
     <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
         <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
@@ -59,144 +57,122 @@
     </a>
 </div>
 
-<div>
-    <div class="row logo-container">
-        <div id="logo" class="col-lg-4">
-            <div class="row no-pad">
-                <img src="/lib/img/episcopalchurchinnavajolandlogo_480.png"
-                     class="img-responsive"  alt="Awesome.jpg">
-            </div>
-        </div>
 
 
-        <div id="title" class="col-lg-6">
-            <div class="text-center" id="title">
-                <h1>Welcome to the Utah Region</h1>
-            </div>
-        </div>
+<div class="container">
+    <div class="text-center">
+        <h1>Welcome to the Utah Region</h1>
     </div>
 </div>
+
+<div class="row logo-container">
+    <div id="logo" class="col-lg-4">
+        <div class="row no-pad">
+            <img src="/lib/img/episcopalchurchinnavajolandlogo_480.png"
+                 class="img-responsive"  alt="Awesome.jpg">
+        </div>
+    </div>
+
+    <div class="col-lg-offset-4 col-lg-4">
+        <a href="#bulletin" class="btn btn-lg btn-primary text-center">Bulletins</a>
+    </div>
+</div>
+
 
 
 <div>
     <div class="container bulliten" id="bulliten">
         <div class="row bulliten">
-
             <div class="col-lg-4" id="rcorners">
                 <h2>St. Christopher's Bee Project is Happening</h2>
                 <h4>100 Pounds is Ready to be Packaged!</h4>
                 <h6>Contact office for more Information</h6>
             </div>
-
             <div class="col-lg-4" id="rcorners">
                 <h2>Staff meeting EVERY Tuesday at 9am, St. Christopher's</h2>
                 <h4></h4>
                 <h6>Contact office for more Information</h6>
             </div>
-
             <div class="col-lg-4" id="rcorners">
                 <h2>40th Annual Convocation was a Success!</h2>
                 <h4>Hosted by the San Juan Region @
                     All Saints Church Farmington, NM</h4>
                 <h6>Thank You For Your Support!</h6>
             </div>
-
         </div>
     </div>
 </div>
 
-<div class="row">
-    <div class="col-lg-4">
-        <div class="thumbnail">
-            <h2>St. Christopher</h2>
-            <img src="/lib/img/stChristopher.jpg" alt="...">
-            <h4>St. Christopher's in Bluff, Utah was built in 1943. It's located 1.7 miles east of the Bluff on Utah
+
+<br>
+<br>
+
+<div class="row" id="history">
+    <div class="col-md-4">
+        <h1>St.Christopher History</h1>
+        <img src="/lib/img/stChris1943.jpg" alt="..." width="400" height="200" img style="border:5px solid white">
+        <h2>St. Christophers</h2>
+        <p>St. Christopher's in Bluff, Utah was built in 1943 . It's located 1.7 miles east of the Bluff on Utah
             highway 162. The grounds also includes a museum and community farm. A free well on the mission ground is used
-            today by many Navajo, as their main water source.</h4>
-        </div>
+            today by many Navajo, as their main water source.</p>
     </div>
-
-    <div class="row">
-        <div class="col-lg-4">
-            <div class="thumbnail">
-                <h2>St. Mary's of the Moonlight</h2>
-                <img src="/lib/img/stMarys.jpg" alt="...">
-                <h4>In the heart of Monument Valley, on the Utah section of the Navajo Reservation, St. Mary's of the
-                Moonlight is there.</h4>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-lg-4">
-                <div class="thumbnail">
-                    <h2>St. John the Baptizer</h2>
-                    <img src="/lib/img/stJohns.jpg" alt="...">
-                    <h4>The Episcopal church, St. John's the Baptizer, is located in the city of Montezuma Creek. Montezuma
-                    Creek is located in the beautiful state of Utah.</h4>
-                </div>
-            </div>
-        </div>
+    <div class="col-md-4">
+        <h1>St. Mary's of the Moonlight History</h1>
+        <img src="/lib/img/stMarys1966.jpg" alt="..." width="400" height="200" img style="border:5px solid white">
+        <h2>St. Mary's of the Moonlight</h2>
+        <p>In 1966, Father Liebler retired and built St. Mary's of the Moonlight.
+            A few miles west of the Oljato trading post at Monument Valley, 60 miles south of Bluff, on the Arizona border.</p>
+    </div>
+    <div class="col-md-4">
+        <h1>St. John the Baptizer</h1>
+        <img src="/lib/img/stJohns1965.jpg" alt="..." width="400" height="200" img style="border:5px solid white">
+        <h2>St. John the Baptizer</h2>
+        <p>The Episcopal church, St. John the Baptizer, is located in the city of Montezuma Creek. Montezuma
+            Creek is located in the beautiful state of Utah.</p>
     </div>
 </div>
 
 
-<div class="text-center" id="info">
-    <p></p>
-</div>
+
+
 
 <div class="row" id="learn-more">
-
-            <div class="col-lg-4">
-                <h3>St. Christopher's</h3>
-                <h6>P.O. Box 28</h6>
-                <h6>Bluff, UT 84512</h6>
-                <a href="http://www.facebook.com"> <i class="fa fa-facebook-official fa-2x"></i></a>
-
-                <dl>
-                    <dt>Rev. Deacon Leon Sampson</dt>
-                    <dd>Cell No. (435)-444-0485</dd>
-                    <dd>Email: sampsonl74@yahoo.com</dd>
-                    <dd>Head Deacon in Charge</dd>
-                </dl>
-
-            </div>
-
-
-            <div class="col-lg-4">
-                <h3>St. Mary's of the Moonlight</h3>
-                <h6>P.O Box 360397</h6>
-                <h6>Oljato, UT 84536</h6>
-                <h6>Office:(435) 727-0200</h6>
-
-                <a href="http://www.facebook.com"> <i class="fa fa-facebook-official fa-2x"></i></a>
-
-                <dl>
-                    <dt>Rev. Catherine Plummer</dt>
-                    <dd>Cell No. (435) 979-5611</dd>
-                    <dd>Priest In Charge at St. Mary's</dd>
-                    <br>
-                </dl>
-            </div>
-
-
-            <div class="col-lg-4">
-                <h3>St. John the Baptizer</h3>
-                <h6>P.O. Box 28</h6>
-                <h6>Montezuma Creek, UT 84534</h6>
-                <h6>Office No. (435) 651-3225</h6>
-                <a href="http://www.facebook.com"> <i class="fa fa-facebook-official fa-2x"></i></a>
-
-                <dl>
-                    <dt>Lillie Henderson</dt>
-                    <dd>Cell No. (435) 444-0484</dd>
-                    <dd>Lay Pastor/Children's Ministry Director</dd>
-                </dl>
-
-            </div>
-        </div>
-
-
-</div>
+    <div class="col-lg-4">
+        <h3>St. Christopher's</h3>
+        <h6>P.O. Box 28</h6>
+        <h6>Bluff, UT 84512</h6>
+        <a href="http://www.facebook.com"> <i class="fa fa-facebook-official fa-2x"></i></a>
+        <dl>
+            <dt>Rev. Deacon Leon Sampson</dt>
+            <dd>Cell No. (435)-444-0485</dd>
+            <dd>Email: sampsonl74@yahoo.com</dd>
+            <dd>Head Deacon in Charge</dd>
+        </dl>
+    </div>
+    <div class="col-lg-4">
+        <h3>St. Mary's of the Moonlight</h3>
+        <h6>P.O Box 360397</h6>
+        <h6>Oljato, UT 84536</h6>
+        <h6>Office:(435) 727-0200</h6>
+        <a href="http://www.facebook.com"> <i class="fa fa-facebook-official fa-2x"></i></a>
+        <dl>
+            <dt>Rev. Catherine Plummer</dt>
+            <dd>Cell No. (435) 979-5611</dd>
+            <dd>Priest In Charge at St. Mary's</dd>
+        </dl>
+    </div>
+    <div class="col-lg-4">
+        <h3>St. John the Baptizer</h3>
+        <h6>P.O. Box 28</h6>
+        <h6>Montezuma Creek, UT 84534</h6>
+        <h6>Office No. (435) 651-3225</h6>
+        <a href="http://www.facebook.com"> <i class="fa fa-facebook-official fa-2x"></i></a>
+        <dl>
+            <dt>Lillie Henderson</dt>
+            <dd>Cell No. (435) 444-0484</dd>
+            <dd>Lay Pastor/Children's Ministry Director</dd>
+        </dl>
+    </div>
 </div>
 
 <ol class="breadcrumb">
@@ -207,6 +183,5 @@
     <li><a href="missionsRetreats.php">Missions/Retreats</a></li>
     <li class="active"></li>
 </ol>
-
 </body>
 </html>
