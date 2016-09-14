2ab5983555c39dbeb52fa621a0de467f599af348
Ryan Sam <rnsam@asu.edu>
2016-09-14 10:04:16 -0600

    Made changes to Carousel and added buttons to index

--- index.php
+++ index.php
@@ -12,97 +12,115 @@
 </head>
 <body>
 <!---------Beginning Carousel ----------------------------------------------------------------------------------------->
-    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-        <!-- Indicators -->
-        <ol class="carousel-indicators">
-            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="3"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="4"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="5"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="6"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="7"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="8"></li>
-        </ol>
+<div class="row " id="carousel">
+    <div class="col-md-8">
 
-        <!-- Wrapper for slides -->
+        <div id="carousel-example-generic" class="carousel slide container" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="5"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="6"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="7"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="8"></li>
+                <li data-target="#carousel-example-generic" data-slide-to="9"></li>
+            </ol>
+
+
+
+            <!-- Wrapper for slides -->
         <div class="carousel-inner" role="listbox">
             <div class="item active">
                 <img src="/lib/img/oldHosp.jpg" alt="...">
-                <div class="carousel-caption">
-                    <h1>Hozho Center</h1>
-                </div>
+                    <div class="carousel-caption">
+                        <h1>Hozho Center</h1>
+                    </div>
             </div>
             <div class="item">
                 <img src="/lib/img/oldHosInterior.jpg" alt="...">
-                <div class="carousel-caption">
-                    <h1>Hozho Center Chapel</h1>
-                </div>
+                    <div class="carousel-caption">
+                        <h1>Hozho Center Chapel</h1>
+                    </div>
             </div>
             <div class="item">
                 <img src="/lib/img/WRYouth.jpg" alt="...">
-                <div class="carousel-caption">
-                    <h1>Southeast Region Youth</h1>
-                </div>
+                    <div class="carousel-caption">
+                        <h1>Southeast Region Youth</h1>
+                    </div>
             </div>
             <div class="item">
                 <img src="/lib/img/GSMBell.JPG" alt="...">
-                <div class="carousel-caption">
-                    <h1>Good Shepherd Mission Bell</h1>
-                </div>
+                    <div class="carousel-caption">
+                        <h1>Good Shepherd Mission Bell</h1>
+                    </div>
             </div>
             <div class="item">
                 <img src="/lib/img/HDF.JPG" alt="...">
-                <div class="carousel-caption">
-                    <h1> Homer Dell Farm</h1>
-                </div>
+                    <div class="carousel-caption">
+                        <h1> Homer Dell Farm</h1>
+                    </div>
             </div>
             <div class="item">
                 <img src="/lib/img/lilyFlower.JPG" alt="...">
-                <div class="carousel-caption">
-                    <h1>St. Christopher's Lily</h1>
-                </div>
+                    <div class="carousel-caption">
+                        <h1>St. Christopher's Lily</h1>
+                    </div>
             </div>
             <div class="item">
                 <img src="/lib/img/ruinsCDc.JPG" alt="...">
-                <div class="carousel-caption">
-                    <h1> Canyon Ruins</h1>
-                </div>
+                    <div class="carousel-caption">
+                        <h1> Canyon Ruins</h1>
+                    </div>
             </div>
             <div class="item">
                 <img src="/lib/img/stCBell.JPG" alt="...">
-                <div class="carousel-caption">
-                    <h1>St. Christopher's Bell</h1>
-                </div>
+                    <div class="carousel-caption">
+                        <h1>St. Christopher's Bell</h1>
+                    </div>
             </div>
             <div class="item">
                 <img src="/lib/img/windowR.JPG" alt="...">
-                <div class="carousel-caption">
-                    <h> WindowRock Monument</h>
-                </div>
+                    <div class="carousel-caption">
+                        <h1> WindowRock Monument</h1>
+                    </div>
             </div>
-            ...
         </div>
 
-        <!-- Controls -->
-        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
+
+
+            <!-- Controls -->
+            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+
     </div>
+
+    <div class="col-md-4" id="buttons">
+        <span style="display: inline-block; width: 100px; vertical-align: top;">
+            <a type="button" href="#bulletin" class="btn btn-default" style="width: 200px; height: 90px;" >Bulletin</a>
+            <a type="button" href="bishopsPage.php" class="btn btn-default" style="width: 200px; height: 90px;">Bishop's Page</a>
+            <a type="button" href="howToHelp.php" class="btn btn-default" style="width: 200px; height: 90px;" >How to Help</a>
+            <a type="button" href="" class="btn btn-default" style="width: 200px; height: 90px;" >Newsletter</a>
+            <a type="button" href="" class="btn btn-default" style="width: 200px; height: 90px;" >ECN</a>
+            <a type="button" href="http://www.cheiidevshop.com/" class="btn btn-default" style="width: 200px; height: 90px;" >Cheii's Web Development</a>
+        </span>
+    </div>
+</div>
+
 <!---------End Carousel ----------------------------------------------------------------------------------------------->
 
 <!-------- Navajoland logo and bulletin button on home page------------------------------------------------------------>
-    <div class="row logo-container" id="indexLogo">
-        <div class="col-lg-offset-8 col-lg-4">
-            <a href="#bulletin" class="btn btn-lg btn-primary text-center">Bulletins</a>
-        </div>
-    </div>
+
 <!-------- End Navajoland logo and bulletin button on home page-------------------------------------------------------->
 
 
